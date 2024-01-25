874801c4ae7300351a6caeef0caf7f71169df078
Kenneth Muriuki <kogimurio@gmail.com>
2024-01-25 06:54:24 +0300

    work on password reset

--- djangoProjectGoogleLogin/settings.py
+++ djangoProjectGoogleLogin/settings.py
@@ -164,3 +164,12 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
+
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'kogimurio@gmail.com'
+EMAIL_HOST_PASSWORD = 'kogi@murio'
--- google_login/urls.py
+++ google_login/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .import views
 
 urlpatterns = [
@@ -6,4 +7,9 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile_view, name='profile'),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='google_login/password_reset.html'), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='google_login/password_reset_sent.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='google_login/password_reset_form.html'), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='google_login/password_reset_done.html'), name='password_reset_complete'),
 ]
